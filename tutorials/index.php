b8d703f62e479850bf361f931259c5d1c70db7af
RayhaanT <tanweer.rayhaan@gmail.com>
2020-06-04 21:45:15 -0400

    some files

--- tutorials/index.php
+++ tutorials/index.php
@@ -1,36 +1,9 @@
 <?php
-    define('CONSTANT', 'pp');
-    $array = [123, 175, 147, 58];
-    echo "\n";
-    echo "<br>";
-    print_r($array);
-
-    function testFunction($boy) {
-        return "{$boy['name']} is {$boy['age']}";
-    }
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Sample PHP file</title>
-</head>
-
-<body>
-    <h2><?php
-        echo 'hello, boys';
-    ?>
-    </h2>
-
-    <div>e
-        <?php
-            echo testFunction(['name' => 'dd', 'age' => '12']);
-        ?>
-    </div>
-    <div>
-        <?php
-            echo testFunction(['name' => 'dd', 'age' =>'13']);
-        ?>
-    </div>
-
-</body>
+<?php
+    require('templates/header.php');
+    require('templates/footer.php');
+?>
 </html>
